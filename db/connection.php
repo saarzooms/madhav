b21da28b8b224c898f15cd02fb04da3eb30977fd
Shamsaagazarzoo M Shaikh <arzoo.rkcet@gmail.com>
2018-02-28 17:32:18 +0530

    update

--- db/connection.php
+++ db/connection.php
@@ -1,12 +1,12 @@
 <?php
 //---------------------------mysql db connection ------------//
 //include('db/connection.php');
-$hostname='localhost';
-$username='i3175962_ally';
-$password=')Ee#L9p8zaI-';
+$hostname='us-cdbr-iron-east-05.cleardb.net';
+$username='b9ff431d15b7e3';
+$password='9194064e';
 
 try {
-    $dbh = new PDO("mysql:host=$hostname;dbname=i3175962_loan",$username,$password);
+    $dbh = new PDO("mysql:host=$hostname;dbname=heroku_859b66fb95bada4",$username,$password);
 //    echo 'Connected to Database<br/>';
     }
 catch(PDOException $e)
